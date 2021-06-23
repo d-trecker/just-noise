af2f309d7c8b0610eaa75d2eb66a8a545d1dad1a
Dylan Trecker <79778111+d-trecker@users.noreply.github.com>
2021-06-22 19:33:05 -0700

    added css, fixed templates, touched up routes and models

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+const withAuth = require('../utils/auth');
 const { Post, User, Comment } = require("../models");
 
-router.get("/", (req, res) => {
-  console.log(req.session);
+router.get("/", withAuth, (req, res) => {
   Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
     attributes: [
       "id",
       "post_content",
@@ -26,12 +30,9 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-        const posts = dbPostData.map(post=> post.get({plain: true}));
-      //pass a single post object into the homepage template data
-      res.render("homepage", {
-        posts,
-        loggedIn: req.session.loggedIn
-      });
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
@@ -39,19 +40,8 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-    res.render('login');
-});
-
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
     attributes: [
       'id',
       'post_content',
@@ -74,21 +64,18 @@ router.get('/post/:id', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
       }
-
-      // serialize the data
-      const post = dbPostData.get({ plain: true });
-      // pass data to template
-      res.render('single-post', { 
-        post,
-        loggedIn: req.session.loggedIn
-       });
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json(err);
     });
 });
