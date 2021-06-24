9fc7a0473772acf6279b2dcdc1127820fee5bb38
Dylan <79778111+d-trecker@users.noreply.github.com>
2021-06-23 23:08:33 -0700

    added commented out get genre route for specific genre page in homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -91,4 +91,39 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
+//----Get all genre route----
+// router.get('/genre/:genre', (req, res) => {
+//   Post.findAll({
+//     attributes: [
+//       'id',
+//       'post_content',
+//       'title',
+//       'created_at'
+//     ],
+//     order: [['created_at', 'DESC']],
+//     where: { post_genre: req.params.genre },
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//         include: {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       },
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => res.json(dbPostData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+
+
 module.exports = router;
