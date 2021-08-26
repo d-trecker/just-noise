09a9b83e51a8ba87645da2d6a970efa8f5c9d54d
ME <mebener44@gmail.com>
2021-08-26 10:10:08 -0700

    updated Index.js in models

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
-// create associations234
+// create associations2344
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
